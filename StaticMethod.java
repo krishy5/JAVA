36562025b7fb2b3157115dd1d99cc0c850e025bc
krishy5 <krishtiwari040@gmail.com>
2026-07-30 18:37:00 +0530

    new code

--- StaticMethod.java
+++
@@ -1,9 +0,0 @@
-public class StaticMethod {
-
-    static void cc() { // can be directly called
-        System.out.println("In method CC");
-    }
-    public static void main(String[] args) {
-        cc();
-    }
-}
